c4c06bb300cf04cb0e4607cfd031c80648f39bb0
Hugo Ferreira <hugo@ferreira.cc>
2016-04-17 22:57:06 +0100

    2.1: Fibonacci refactor scalacheck properties
    Separate each assertion concern into a separate property of the
    fibonacci test with numbers Generator.

--- src/test/scala/fpinscala/ch02gettingstarted/GettingStartedSpec.scala
+++ src/test/scala/fpinscala/ch02gettingstarted/GettingStartedSpec.scala
@@ -7,37 +7,47 @@ import org.scalatest.prop.PropertyChecks
 import scala.collection.immutable.{BitSet, HashSet, TreeSet}
 
 class GettingStartedSpec extends WordSpec with PropertyChecks with Matchers {
+  import MyModule._
 
-  "fibonacci (with scalacheck experiments)" must {
+  "fibonacci (with scalacheck Tables)" must {
     val fibonacci = Seq(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233)
 
     "match the calculated fib number in a results Table" in {
       val values = Table(("fib", "num"), fibonacci.zipWithIndex: _*)
-      forAll(values) { (fib, num) =>
-        MyModule.fib(num) shouldBe fib
-        MyModule.fib_non_tailrec(num) shouldBe fib
+      forAll(values) { (x, n) =>
+        fib(n) shouldBe x
+        fib_non_tailrec(n) shouldBe x
       }
     }
 
     "match the index Generator in a results Table" in {
       val values = Table("fib", fibonacci: _*)
       val indexes = Gen.choose(0, values.length - 1)
-      forAll((indexes, "num")) { n => MyModule.fib(n) shouldBe values(n) }
+      forAll((indexes, "num")) { n => fib(n) shouldBe values(n) }
     }
+  }
+
+  "fibonacci (with scalacheck Generator)" must {
+    // Gen.posNum[Int] goes from 1...Int.MaxValue which causes `fib` to go around to negative values
+    val smallNonNegativeNums = Gen.choose(0, 42)
 
     // Adapted from “QuickCheck in Action” in:
     // [A brief intro to QuickCheck | Stuart Gunter](http://www.stuartgunter.org/intro-to-quickcheck/)
-    "comply with fib(n) + fib(n + 1) = f(n + 2) for non-negative numbers Generator" in {
-      // Gen.posNum[Int] goes from 1...Int.MaxValue which goes around to negative values
-      forAll((Gen.choose(0, 42), "num")) { n: Int =>
-        val x = MyModule.fib(n)
-        val y = MyModule.fib(n + 1)
-        val z = MyModule.fib(n + 2)
-        x should be >= 0
-        y should be >= 0
-        z should be >= 0
-        x should (be < y or be < z)
-        x + y shouldEqual z
+    "comply with the rule fib(n) + fib(n+1) = f(n+2)" in {
+      forAll((smallNonNegativeNums, "num")) { n: Int =>
+        fib(n) + fib(n + 1) shouldEqual fib(n + 2)
+      }
+    }
+
+    "always be a non-negative value" in {
+      forAll((smallNonNegativeNums, "num")) { n: Int =>
+        fib(n) should be >= 0
+      }
+    }
+
+    "be strictly ordered fib(n) < fib(n+1) for every n != 1" in {
+      forAll((smallNonNegativeNums, "num")) { n: Int =>
+        whenever(n != 1)(fib(n) should be < fib(n + 1))
       }
     }
   }
