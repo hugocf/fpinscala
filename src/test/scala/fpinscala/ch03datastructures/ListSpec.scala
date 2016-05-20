355c4237b430b7dfc1182eb475011268f6df4018
Hugo Ferreira <hugo@ferreira.cc>
2016-05-20 10:41:28 +0100

    3.2: tail

---
+++ src/test/scala/fpinscala/ch03datastructures/ListSpec.scala
@@ -0,0 +1,27 @@
+package fpinscala.ch03datastructures
+
+import fpinscala.BaseSpec
+import fpinscala.ch03datastructures.List._
+import org.scalacheck.{Arbitrary, Gen}
+
+class ListSpec extends BaseSpec {
+
+  // See [scala - How to define an arbitrary for a custom list in scalacheck? - Stack Overflow][1]
+  // [1]: http://stackoverflow.com/questions/31878928/how-to-define-an-arbitrary-for-a-custom-list-in-scalacheck
+  private def myConsGen[T:Arbitrary]: Gen[List[T]] = for {
+    head <- Arbitrary.arbitrary[T]
+    tail <- Gen.oneOf(myNilGen, myConsGen[T])
+  } yield Cons(head, tail)
+
+  private val myNilGen: Gen[List[Nothing]] = Gen.delay(Nil)
+
+  implicit def myListArbitrary[T:Arbitrary]: Arbitrary[List[T]] = Arbitrary[List[T]](Gen.oneOf(myNilGen, myConsGen[T]))
+
+  "tail" must {
+    "tail of a concatenation " in {
+      forAll { (x: Int, xs: List[Int]) =>
+        tail(Cons(x, xs)) shouldBe xs
+      }
+    }
+  }
+}
